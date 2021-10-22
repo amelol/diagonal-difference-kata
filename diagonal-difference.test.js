889e4765b18305c6ff0bbea91b2f9663d2baacb7
amelol <75670435+amelol@users.noreply.github.com>
2021-10-22 12:06:32 +0200

    fix: test

--- diagonal-difference.test.js
+++ diagonal-difference.test.js
@@ -2,7 +2,7 @@ let diagonalDifference = require("./diagonal-difference");
 
 describe("testing diagonalDifference", () => {
   test("test if the difference between diagonal sums of 2d matrix is correct", () => {
-    let arr = [11, 2, 4, 4, 5, 6, 10, 8, -12];
+    let arr = [[11, 2, 4], [4, 5, 6], [10, 8, -12]];
     expect(diagonalDifference(arr)).toBe(15)
   })
 })
